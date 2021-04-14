1e6841be62cdd305187fc70f0ba24acb7a2ccd21
Angelo Montini <angelo.montini@gmail.com>
2021-04-14 13:04:03 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
   style: "mapbox://styles/mapbox/light-v10",
   accessToken:
     "pk.eyJ1IjoibWFwbW9saW5hY2giLCJhIjoiY2tuaDVyNjBxM2J1cDJycDlmMjJpczN3dyJ9.hoSf2ecIgPRw1R0ih3txRQ",
-  CSV: "./Sample_Data.csv",
+  CSV: "./data.csv",
   center: [10.234, 47.398],
   zoom: 6,
   title: "Title",
